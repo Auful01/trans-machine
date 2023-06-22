8c7f73fb984250fd43690daf2f4a2aab95f3c433
auful01 <distriq19@gmail.com>
2023-06-22 11:15:25 +0700

    Update

--- public/python/cosine_similarity.py
+++ public/python/cosine_similarity.py
@@ -5,17 +5,17 @@ from nltk.tokenize import word_tokenize
 import sys
 import json
 
-import nltk
+# import nltk
 # import ssl
 
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    pass
-else:
-    ssl._create_default_https_context = _create_unverified_https_context
+# try:
+#     _create_unverified_https_context = ssl._create_unverified_context
+# except AttributeError:
+#     pass
+# else:
+#     ssl._create_default_https_context = _create_unverified_https_context
 
-nltk.download('stopwords')
+# nltk.download('stopwords')
 
 # nltk.download()
 # X = input("Enter first string: ").lower()
