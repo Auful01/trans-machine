ec0219a69a925cab0602e5ff52d0baced8522eb8
auful01 <distriq19@gmail.com>
2023-06-22 07:40:05 +0700

    Update

--- public/python/convert2txt.py
+++ public/python/convert2txt.py
@@ -5,7 +5,7 @@ from deep_translator import GoogleTranslator
 
 import urllib2  # the lib that handles the url stuff
 
-data = urllib2.urlopen(target_url) # it's a file like object and works just like a file
+data = urllib2.urlopen(sys.argv[1]) # it's a file like object and works just like a file
 for line in data: # files are iterable
     print(line)
 
