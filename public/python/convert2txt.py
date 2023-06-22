5f46e2e9472cd0f0ec093a9d0d7eaa4ff89f66e8
auful01 <distriq19@gmail.com>
2023-06-22 07:36:18 +0700

    Update

--- public/python/convert2txt.py
+++ public/python/convert2txt.py
@@ -3,42 +3,48 @@ import PyPDF2
 import spacy
 from deep_translator import GoogleTranslator
 
-translator = GoogleTranslator(source='auto', target='en')
+import urllib2  # the lib that handles the url stuff
+
+data = urllib2.urlopen(target_url) # it's a file like object and works just like a file
+for line in data: # files are iterable
+    print(line)
+
+# translator = GoogleTranslator(source='auto', target='en')
 
 
-nlp = spacy.load("en_core_web_sm")
+# nlp = spacy.load("en_core_web_sm")
 
-path = '/Users/aufulkirom/Documents/KULIAH/SKRIPSI/translation-machine/public/storage/asal/';
+# path = '/Users/aufulkirom/Documents/KULIAH/SKRIPSI/translation-machine/public/storage/asal/';
 
 
-file = open(
-     sys.argv[1], 'rb')
+# file = open(
+#      sys.argv[1], 'rb')
 
-reader = PyPDF2.PdfFileReader(file)
+# reader = PyPDF2.PdfFileReader(file)
 
-rest = sys.argv[1]
-print(rest)
+# rest = sys.argv[1]
+# print(rest)
 
-num_pages = reader.numPages
+# num_pages = reader.numPages
 
-for p in range(num_pages):
-    page = reader.getPage(p)
-    text = page.extractText()
-    results = text.replace(";", "ti")
-    rest += results
+# for p in range(num_pages):
+#     page = reader.getPage(p)
+#     text = page.extractText()
+#     results = text.replace(";", "ti")
+#     rest += results
 
-res = ''
-resTrans = ''
-result = []
-doc = nlp(rest)
+# res = ''
+# resTrans = ''
+# result = []
+# doc = nlp(rest)
 
-idx = 0
+# idx = 0
 
-for sent in doc.sents:
-    trans = translator.translate(sent.text)
-    resTrans = resTrans + (str(idx) + ":" + str(trans)) + ";"
-    res = res + (str(idx) + ":" + str(sent.text)) + ";"
-    idx += 1
+# for sent in doc.sents:
+#     trans = translator.translate(sent.text)
+#     resTrans = resTrans + (str(idx) + ":" + str(trans)) + ";"
+#     res = res + (str(idx) + ":" + str(sent.text)) + ";"
+#     idx += 1
 
-print(res)
-print(resTrans)
+# print(res)
+# print(resTrans)
