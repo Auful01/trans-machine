9146e4ac594dcf4a13a1f9a4024dbbd2846941df
auful01 <distriq19@gmail.com>
2023-06-22 10:59:48 +0700

    Update

--- public/python/jawaban_slice.py
+++ public/python/jawaban_slice.py
@@ -22,7 +22,7 @@ reader = fitz.open(stream=memoryFile, filetype="pdf")
 
 rest = ''
 for page in reader:
-    text = page.getText()
+    text = page.getText(sort=True)
     print(text.encode('utf-8'))
 #     rest += text.replace("Machine Translated by Google",
 #                          "").replace("\n", " ").replace(";", "")
